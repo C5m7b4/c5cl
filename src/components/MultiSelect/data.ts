47b84dc6b4921a934e8237ddc38a1f0b67e7934f
C5m7b4 <mbedingfield@dcrpos.com>
2022-03-10 09:08:44 -0600

    add multiselect

---
+++ src/components/MultiSelect/data.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: 1,
+    value: 'Grocery',
+  },
+  {
+    id: 2,
+    value: 'Nonfood',
+  },
+  {
+    id: 3,
+    value: 'HBA',
+  },
+  {
+    id: 4,
+    value: 'Meat',
+  },
+  {
+    id: 5,
+    value: 'Meat by #',
+  },
+  {
+    id: 6,
+    value: 'Produce',
+  },
+  {
+    id: 7,
+    value: 'Produce by #',
+  },
+];
