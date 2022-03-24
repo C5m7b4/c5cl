37fc770594a9d190d3f466c93a5f036ebf096285
C5m7b4 <mbedingfield@dcrpos.com>
2022-03-24 09:55:17 -0500

    add type and size

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,7 +4,7 @@ import { Story, Meta } from '@storybook/react';
 import Button, { ButtonProps } from './Button';
 
 export default {
-  title: 'Buttons/Button',
+  title: 'Buttons',
   component: Button,
 } as Meta;
 
